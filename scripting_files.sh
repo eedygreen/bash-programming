1c8c14d2c21649589857e3f47fb1c5941ffb7b75
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-07-09 07:27:08 +0100

    scripting append

--- scripting_files.sh
+++ scripting_files.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
 
+# this script checks for a valid file and permission and append the text to the file
+
 echo -e "Enter the name of the file: \c"
 
 read file_name
 
 if [ -f $file_name ]
 then
-    echo "its a valid file"
+    if [ -w $file_name ]
+    then
+        echo -e "Enter the texts or press ctrl + d to quit: \c"
+        cat >> $file_name
+    else
+        echo "the file does not have a write permission"
+    fi
 else
     echo "its not a valid file"
 fi
