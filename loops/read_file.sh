56a9871b60770a37e76ce82afb5398c2d91ab652
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-07-17 13:50:09 +0100

    fixed globbing

--- loops/read_file.sh
+++ loops/read_file.sh
@@ -2,10 +2,10 @@
 
 echo -e "Enter the file name: \c"
 
-read file_name
+read -r file_name
 # good practice to read file is to use Internal Field Separator 'IFS'
 
-while IFS= read -r line
+while IFS= read -r line # use IFS=' ' to explicit specify the white space separator
 do
-    echo $line
-done < $file_name
+    echo "$line"
+done < "$file_name"
