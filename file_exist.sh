ee4813ae379508ecbf90c9e6a76a250701a2624f
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-07-18 22:38:47 +0100

    finding files

---
+++ file_exist.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+is_file_exist() {
+    local file_name=$1
+    [[ -f "$file_name" ]] && return 0 || return 1
+}
+
+usage() {
+    echo "You must provide a file name: "
+    echo "usage $0 file_name"
+}
+
+usage
+
+if ( is_file_exist "$1" )
+then 
+    echo "file found"
+else
+    echo "file not found!"
+fi
