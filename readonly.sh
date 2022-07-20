628ca570dc2e2621550b07eceb1da07c8da3eb44
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-07-20 08:39:01 +0100

    fixed

--- readonly.sh
+++ readonly.sh
@@ -6,9 +6,6 @@
 readonly var=10
 echo "$var"
 
-# with readonly this function can be called but cannot be overwritten
-try_it
-
 try_it() {
     echo "Just testing readonly"
 }
@@ -16,10 +13,13 @@ try_it() {
 # use -f for functions
 readonly -f try_it
 
+# with readonly this function can be called but cannot be overwritten
+try_it
+
 # this will throw an error: readonly function
-try_it() {
-    echo "testing to see if it works"
-}
+# try_it() {
+#     echo "testing to see if it works"
+# }
 
 readonly        # this will print out the readonly variables. readonly -p is same as readonly
 readonly -f     # this will print out the successful readonly functions
