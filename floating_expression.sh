e949c85c413e2a3c3927611ace150a81072f3473
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-07-22 22:45:12 +0100

    roundup to 3 decimal places (#23)
    * roundup to 3 decimal places
    
    * use * instead of @ and quoting to prevent globbing

---
+++ floating_expression.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# A mathematical expression containing +,-,*,^, / and parenthesis will be provided. Read in the expression, 
+# then evaluate it. Display the result rounded to  decimal places.
+
+# constraints
+
+# All numeric values are <= 999.
+
+read -ra express
+
+printf "%.3f \n" "$( echo "scale=4;${express[*]}" | bc )" # best practice use * instead of @ for arrays
