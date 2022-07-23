a7f1f382723dde0408f3ca15c30180a9843905be
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-07-23 10:04:36 +0100

    calculate average from STDIN (#24)
    * calculate average from STDIN
    
    * fixed
    
    * fixed quoting unstable issues
    
    * fixed Quoting error

---
+++ average.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Given  integers, compute their average, rounded to three decimal places.
+
+# Input Format
+# The first line contains an integer, .
+# Each of the following  lines contains a single integer.
+
+# Output Format
+# Display the average of the  integers, rounded off to three decimal places.
+
+# Input Constraints
+
+# ( refers to elements of the list of integers for which the average is to be computed)
+
+IFS=" " read -r num_of_lines        # number of lines as the total number of give integers
+# items=( $(cat) )                    # this will pass all the input in an arrary
+mapfile -t items  <<< "$(cat)"
+printf "%.3f \n" "$(echo "scale=4;$((${items[*]/%/ +}0))"/"$num_of_lines" | bc )"
