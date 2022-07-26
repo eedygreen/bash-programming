3515bd3334a6ca1c52ff9cb651a2bbe99461ba01
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-07-26 13:14:43 +0100

    cutting characters to end of text (#27)

---
+++ cut_3.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+file_name=/Users/isahidris/Documents/GitHub/bash-programming/files
+
+# print the 25th character to end of the text
+
+text_ipnut() {
+    cut -c 2- $file_name
+}
+
+text_ipnut
