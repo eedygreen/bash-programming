0a49e840a4f18f265498cd5b79aac91400321330
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-07-27 06:32:24 +0100

    Head (#30)
    * sorting is fun
    
    * fixed globbing
    
    * used internal field separator

---
+++ head.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# display the lines (from line number 12 to 22, both inclusive) of a given text file.
+
+# best case
+
+head -n 22 | tail -n +12
+
+# second best case
+touch file
+head -n 22 file >> file_1
+tail -n 11 file_1
+
+# third worst case
+for ((n=0; n<=22; n++))
+do
+    IFS= read -r
+
+    if [ "$n" -ge 12 ] 
+    then
+        echo "${REPLY}"
+    fi
+done
