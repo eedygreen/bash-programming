6c536faf56526508db4bbde97fd0ca4d111de318
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-08-02 23:30:49 +0100

    Awk (#37)
    * sorting is fun
    
    * regex
    
    * printing with awk
    
    * more on awk

--- awk.sh
+++ awk.sh
@@ -11,3 +11,27 @@ awk '{ print $1,$2 }' text_file.csv # print column 1 & 2
 
 # join two columns together
 awk '{ print $1.$2 }' text_file.csv # this cancatenate column and 2
+
+# print sepcific word
+awk '/find it/ { print }' text_file.csv # its case sensitve
+
+# every line that contains lower case
+awk '/[a-z]/ { print }' text_file.csv
+
+# every line that contains numbers
+awk '/[0-9]/ { print }' text_file.csv
+
+# every line that starts with numbers
+awk '/^[0-9]/ { print }' text_file.csv
+
+# every line that ends with numbers
+awk '/[0-9]$/ { print }' text_file.csv
+
+# awk works with conditionals
+awk '{ if( $1 ~ /234/ ) print }' text_file.csv
+
+# check if its a number
+awk '{ if( $2 ~ /[0-9]/ ) print }' text_file.csv
+
+# field delimiter
+awk -F: '{ print }' text_file.csv
