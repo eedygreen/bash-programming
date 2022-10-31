c5cdb4eb51c2cfe141b383189c12c9ff5d1d620e
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-10-31 12:53:55 +0100

    ommitted changes

--- loops/until_loop.sh
+++ loops/until_loop.sh
@@ -7,14 +7,14 @@ IFS= read -r age
 # The untill loop is the same as while loop but it operates with the condition of false
 # and stops when the condition is true
 
-until [[ "$age" -lt 10 ]] # if age is greater than 10
+until [[ $age -lt 10 ]] # if age is greater than 10
 do 
     echo "If the number you entered is greater than 10: $age"
     (( age-- ))
 done
 
-until [[ "$age" -gt 10 ]] # if age is less than 10
-do 
-    echo "If the number you entered is less than 10: $age"
-    (( age++ ))
-done
+# until [[ $age -gt 10 ]] # if age is less than 10
+# do 
+#     echo "If the number you entered is less than 10: $age"
+#     (( age++ ))
+# done
